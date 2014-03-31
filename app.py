bc85221bca851a8dcf8a53b76782be4b3910b53f
Shane Carveo <mixedpuppy@gmail.com>
2014-03-31 11:13:59 -0700

    work on urls

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 import re, os, sys
 from flask import Flask, request, json, render_template, redirect
+from werkzeug.routing import BaseConverter
 from pystache.renderer import Renderer
 import collections
+
+class RegexConverter(BaseConverter):
+    def __init__(self, url_map, *items):
+        super(RegexConverter, self).__init__(url_map)
+        self.regex = items[0]
 
 def createapp():
   app = Flask(__name__, static_folder='')
   app.debug = True
+
+  app.url_map.converters['regex'] = RegexConverter
 
   data = json.load(app.open_resource('js/data.json'), object_pairs_hook=collections.OrderedDict)
 
@@ -44,7 +52,6 @@ def createapp():
       return render_template(template, **d)
     else:
       for k, p in data["source"].iteritems():
-        p['key'] = k
         dataFixup(k, p, locale)
 
       if locale in data["carousel"]:
@@ -57,6 +64,39 @@ def createapp():
   
       return render_template(template, **data)
   
+  @app.route('/<regex("\w{2}_\w{2}"):locale>/<path>')
+  def static_proxy(locale, path):
+    # if root is locale, capture that, but use the same local file paths
+    print "root in ", locale
+    print "path in ", path
+    try:
+      root, path = path.split('/', 2)
+    except ValueError, e:
+      root = None
+    print "locale is ", locale
+    print "root is ", root
+    print "path is ", path
+    # send_static_file will guess the correct MIME type
+    if root in ["css", "images", "fonts", "js"]:
+      return app.send_static_file(os.path.join(root, path))
+    if path == "index.html":
+      template = path
+    else:
+      template = path and "provider.html" or "index.html"
+    print "template is ", template
+    return renderTemplate(template, dict(data), locale, path)
+
+  @app.route('/<regex("\w{2}_\w{2}"):locale>/<path:path>')
+  def static_files(locale, path):
+    return app.send_static_file(path)
+  
+
+  @app.route('/<regex("\w{2}_\w{2}"):locale>/')
+  def rootname(locale):
+    print "rendering ", locale
+    # if root is locale, capture that, but use the same local file paths
+    return renderTemplate('index.html', dict(data), root)
+
   @app.route('/')
   def root():
     # the server should handle a redirect. Since the frozen static pages will be
@@ -64,27 +104,5 @@ def createapp():
     # level index page.  We use that here to ensure that works as well.
     return app.send_static_file("index.html")
 
-  @app.route('/<root>/')
-  def rootname(root):
-    print "rendering ", root
-    # if root is locale, capture that, but use the same local file paths
-    if re.search('\w{2}_\w{2}', root):
-      return renderTemplate('index.html', dict(data), root)
-    return renderTemplate('provider.html', dict(data), "en_US", root)
-
-  @app.route('/<root>/<path:path>')
-  def static_proxy(root, path):
-    # if root is locale, capture that, but use the same local file paths
-    locale = "en_US"
-    if re.search('\w{2}_\w{2}', root):
-      locale = root
-      path = path.split("/")[0]
-      print "path is ", path
-    # send_static_file will guess the correct MIME type
-    if root in ["css", "images", "fonts", "js"]:
-      return app.send_static_file(os.path.join(root, path))
-    template = path and "provider.html" or "index.html"
-    print "template is ", template
-    return renderTemplate(template, dict(data), locale, path)
 
   return app
--- freeze.py
+++ freeze.py
@@ -4,4 +4,14 @@ from app import createapp;
 if __name__ == '__main__':
   app = createapp()
   freezer = Freezer(app)
+
+  #@freezer.register_generator
+  #def product_url_generator():
+  #    # Return a list. (Any iterable type will do.)
+  #    return [
+  #        '/',
+  #        '/en_US/',
+  #        '/js_JP/mixi',
+  #    ]
+
   freezer.freeze()
